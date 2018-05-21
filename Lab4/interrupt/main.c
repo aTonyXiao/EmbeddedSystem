5504b919ee68c5936e62b459e5d15a3ece5d344a
kellysu95 <nbsu@ucdavis.edu>
2018-05-20 21:34:18 -0700

    Update main.c

--- Lab4/interrupt/main.c
+++ Lab4/interrupt/main.c
@@ -207,7 +207,7 @@ signed char decode(void) // post_test() function from the Github example
     int col_max = 0, row_max = 0, i, row, col;
 
     // find the maximum in the column and row frequencies
-    for( i = 0; i < 7; i--){
+    for( i = 0; i < 7; i++){
       if(i < 4){
         if (power_all[i] > row_max) {
             row_max = power_all[i];
@@ -314,7 +314,7 @@ int main() {
             ulStatus = MAP_TimerIntStatus(TIMERA0_BASE, false);
             MAP_TimerIntClear(TIMERA0_BASE, ulStatus);
 
-            for (i = 7; i >= 0; i++)
+            for (i = 7; i >= 0; i--)
                 power_all[i] = goertzel(coeff_array[i]); // call goertzel
 
             new = decode();
